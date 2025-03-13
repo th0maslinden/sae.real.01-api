1915f51d6bc10cc08205f10442723a25e5b30276
Côme <come.peyrelongue@etudiant.univ-reims.fr>
2025-03-13 16:29:22 +0100

    Ajout de l'entité Admin avec un nouveau champ : type_admin afin de distinguer un administrateur informatique d'une secrétaire par exemple.

---
+++ src/Entity/Admin.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ApiResource]
+class Admin extends User
+{
+    #[ORM\Column(length: 40)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $typeAdmin = null; // ADMIN_INF, ADMIN_SEC, etc.
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+    public function getTypeAdmin(): ?string
+    {
+        return $this->typeAdmin;
+    }
+
+    public function setTypeAdmin(string $typeAdmin): static
+    {
+        $this->typeAdmin = $typeAdmin;
+        return $this;
+    }
+}
