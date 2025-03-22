5c3f06fa082e49ea4b8fc5b97a388b5a32c43616
Côme <come.peyrelongue@etudiant.univ-reims.fr>
2025-03-22 15:49:50 +0100

    Modification importante de l'API : étape 3 : ajout des entités

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,33 +2,25 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Patch;
-use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\Put;
 use ApiPlatform\OpenApi\Model\Operation;
 use App\Controller\GetAvatarController;
 use App\Repository\UserRepository;
 use App\State\MeProvider;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\InheritanceType('SINGLE_TABLE')]
-#[ORM\DiscriminatorColumn(name: 'dtype', type: 'string')]
-#[ORM\DiscriminatorMap([
-    'user' => User::class,
-    'patient' => Patient::class,
-    'professionnel' => Professionnel::class,
-    'admin' => Admin::class,
-])]
-#[ORM\Table(name: 'user')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_LOGIN', fields: ['login'])]
 #[ApiResource(
     operations: [
@@ -78,37 +70,103 @@ use Symfony\Component\Serializer\Attribute\Groups;
     ],
     normalizationContext: ['groups' => ['User_read']]
 )]
+#[ORM\InheritanceType('SINGLE_TABLE')]
+#[ORM\DiscriminatorColumn(name: 'dtype', type: 'string')]
+#[ORM\DiscriminatorMap([
+    'user' => User::class,
+    'patient' => Patient::class,
+    'professionnel' => Professionnel::class,
+    'admin' => Admin::class,
+])]
 #[UniqueEntity('login')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['user:read'])]
-    protected ?int $id = null;
+    #[Groups(['User_read'])]
+    private ?int $id = null;
 
-    #[ORM\Column(length: 180)]
-    #[Groups(['user:read', 'user:write'])]
-    protected ?string $login = null;
+    #[ORM\Column(length: 180, unique: true)]
+    #[Groups(['User_read', 'User_write'])]
+    #[Assert\Regex(
+        pattern: '/[<>\"&]/',
+        message: 'Le nom contient des caractères interdits : <, >, & et "',
+        match: false
+    )]
+    #[ApiProperty(
+        description: 'Login de notre utilisateur.',
+        openapiContext: [
+            'type' => 'string',
+            'example' => 'user1',
+        ]
+    )]
+    private ?string $login = null;
 
+    /**
+     * @var list<string> The user roles
+     */
     #[ORM\Column]
     #[Groups(['user:read'])]
-    protected array $roles = [];
+    private array $roles = [];
 
+    /**
+     * @var string The hashed password
+     */
     #[ORM\Column]
-    #[Groups(['user:write'])]
-    protected ?string $password = null;
+    #[Groups(['User_write'])]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 30)]
+    #[Groups(['User_read', 'User_write'])]
+    #[Assert\Regex(
+        pattern: '/[<>\"&]/',
+        message: 'Le nom contient des caractères interdits : <, >, & et "',
+        match: false
+    )]
+    #[ApiProperty(
+        description: 'Prénom de notre utilisateur.',
+        openapiContext: [
+            'type' => 'string',
+            'example' => 'Solène',
+        ]
+    )]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 40)]
+    #[Groups(['User_read', 'User_write'])]
+    #[Assert\Regex(
+        pattern: '/[<>\"&]/',
+        message: 'Le nom contient des caractères interdits : <, >, & et "',
+        match: false
+    )]
+    #[ApiProperty(
+        description: 'Nom de famille de notre utilisateur.',
+        openapiContext: [
+            'type' => 'string',
+            'example' => 'Depret--Masson',
+        ]
+    )]
+    private ?string $lastname = null;
 
     #[ORM\Column(type: Types::BLOB, nullable: true)]
-    protected $avatar = null;
+    private $avatar;
 
     #[ORM\Column(length: 100)]
-    #[Groups(['user:read', 'user:write'])]
-    protected ?string $email = null;
+    #[Groups(['User_write', 'User_me'])]
+    #[Assert\Email(
+        message: 'The email {{ value }} is not a valid email.',
+    )]
+    private ?string $email = null;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
     }
 
     public function getLogin(): ?string
@@ -119,27 +177,47 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setLogin(string $login): static
     {
         $this->login = $login;
+
         return $this;
     }
 
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
     public function getUserIdentifier(): string
     {
         return (string) $this->login;
     }
 
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
     public function getRoles(): array
     {
         $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
+
         return array_unique($roles);
     }
 
+    /**
+     * @param list<string> $roles
+     */
     public function setRoles(array $roles): static
     {
         $this->roles = $roles;
+
         return $this;
     }
 
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
     public function getPassword(): ?string
     {
         return $this->password;
@@ -148,10 +226,42 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPassword(string $password): static
     {
         $this->password = $password;
+
         return $this;
     }
 
-    public function eraseCredentials(): void {}
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
 
     public function getAvatar()
     {
@@ -161,6 +271,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setAvatar($avatar): static
     {
         $this->avatar = $avatar;
+
         return $this;
     }
 
@@ -172,6 +283,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setEmail(string $email): static
     {
         $this->email = $email;
+
         return $this;
     }
 }
