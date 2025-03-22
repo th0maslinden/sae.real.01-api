1b93d917f4e7d0dc9f2b7ae1ccc8a2fb541bb843
Côme <come.peyrelongue@etudiant.univ-reims.fr>
2025-03-22 15:48:20 +0100

    Modification importante de l'API : étape 1 : mise à jour des versions des packages

--- migrations/Version20250322143653.php
+++ migrations/Version20250322143653.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250312210211 extends AbstractMigration
+final class Version20250322143653 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,7 +23,7 @@ final class Version20250312210211 extends AbstractMigration
         $this->addSql('CREATE TABLE planning (id INT AUTO_INCREMENT NOT NULL, date DATE NOT NULL, heure_debut TIME NOT NULL, heure_fin TIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE ressource (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(40) NOT NULL, type VARCHAR(60) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE seance (id INT AUTO_INCREMENT NOT NULL, patient_id INT DEFAULT NULL, professionnel_id INT DEFAULT NULL, date DATE DEFAULT NULL, heure_debut TIME DEFAULT NULL, heure_fin TIME DEFAULT NULL, note VARCHAR(1000) DEFAULT NULL, raison VARCHAR(50) DEFAULT NULL, INDEX IDX_DF7DFD0E6B899279 (patient_id), INDEX IDX_DF7DFD0E8A49CC82 (professionnel_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, login VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, avatar LONGBLOB DEFAULT NULL, email VARCHAR(100) NOT NULL, dtype VARCHAR(255) NOT NULL, nom VARCHAR(40) DEFAULT NULL, prenom VARCHAR(40) DEFAULT NULL, pathologie VARCHAR(100) DEFAULT NULL, specialite VARCHAR(40) DEFAULT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_LOGIN (login), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, login VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, firstname VARCHAR(30) NOT NULL, lastname VARCHAR(40) NOT NULL, avatar LONGBLOB DEFAULT NULL, email VARCHAR(100) NOT NULL, dtype VARCHAR(255) NOT NULL, pathologie VARCHAR(100) DEFAULT NULL, specialite VARCHAR(40) DEFAULT NULL, type_admin VARCHAR(20) DEFAULT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_LOGIN (login), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE seance ADD CONSTRAINT FK_DF7DFD0E6B899279 FOREIGN KEY (patient_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE seance ADD CONSTRAINT FK_DF7DFD0E8A49CC82 FOREIGN KEY (professionnel_id) REFERENCES user (id)');
     }
